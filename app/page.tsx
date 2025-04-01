3ee18489f9dbfab3a263901daa10d0a97326b074
Maxime CHANEL <max.chanel01@gmail.com>
2025-04-01 08:43:39 +0200

    selection des joueurs et des équipes

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,27 @@
 // pages/index.tsx
-import Link from "next/link";
+
+function MyButton({ title, className }: { title: string; className?: string }) {
+  return <button className={className}>{title}</button>;
+}
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold mb-6">Bienvenue sur notre site</h1>
-      
+
       <div className="flex items-center justify-center w-full max-w-4xl">
         {/* Rectangle bleu */}
         <div className="w-40 h-60 bg-cyan-500"></div>
-        
+
         {/* Conteneur des boutons */}
         <div className="flex flex-col items-center mx-8 space-y-4">
-          <button className="px-4 py-2 w-[227px] bg-gray-200 text-black rounded shadow">
-            ← Rejoindre l'équipe bleu
-          </button>
-          <button className="px-4 py-2 bg-gray-200 text-black rounded shadow">
-            Rejoindre l'équipe rouge →
-          </button>
+          <MyButton
+            title="← Rejoindre l'équipe bleu"
+            className="px-4 py-2 w-[227px] bg-gray-200 text-black rounded shadow"
+          />
+            <button className="px-4 py-2 bg-gray-200 text-black rounded shadow">
+              Rejoindre l'équipe rouge →
+            </button>
         </div>
 
         {/* Rectangle rouge */}
